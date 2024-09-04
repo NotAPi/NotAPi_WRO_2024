bffb7f39c12948d65b44e3dc04581a5f046a5cdd
JaraGuillen <jaraguillen111@gmail.com>
2024-09-04 17:59:51 +0200

    loquesea

--- Code/ArduinoR4/SencilloPeroEfectivo/SencilloPeroEfectivo.ino
+++ Code/ArduinoR4/SencilloPeroEfectivo/SencilloPeroEfectivo.ino
@@ -8,7 +8,7 @@
 #define ECHO_PIN2     4
 #define TRIGGER_PIN3  7
 #define ECHO_PIN3     6
-#define MaxDistance   150
+int MaxDistance = 300;
 
 // Motor Control Pins
 #define IN1_PIN       9
@@ -19,12 +19,16 @@
 Servo myservo;
 #define SERVO_PIN 11
 
+int centro = 110;
+int miligiro = 500;
+int distanciafreno = 50;
+
 NewPing sonarRight(TRIGGER_PIN1, ECHO_PIN1, MaxDistance);
 NewPing sonarFront(TRIGGER_PIN2, ECHO_PIN2, MaxDistance);
 NewPing sonarLeft(TRIGGER_PIN3, ECHO_PIN3, MaxDistance);
 
-void forward() {
-  analogWrite(ENABLE_PIN, 150);
+void forward(int vel) {
+  analogWrite(ENABLE_PIN, vel);
   digitalWrite(IN1_PIN, HIGH);
   digitalWrite(IN2_PIN, LOW);
 }
@@ -35,12 +39,110 @@ void stop() {
   digitalWrite(IN2_PIN, LOW);
 }
 
-void setup() {
-  // put your setup code here, to run once:
+void forceStop() {
+  // Breve inversión del motor para frenar el robot
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+  analogWrite(ENABLE_PIN, 255);  // Ajusta la velocidad al máximo inverso
+  delay(200);  // Pequeño retraso para aplicar el freno
 
+  // Detener el motor completamente
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, LOW);
+  analogWrite(ENABLE_PIN, 0);
+}
+
+
+void setup() { 
+  // Motor Control
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+
+  myservo.attach(SERVO_PIN);
+
+  Serial.begin(9600);
+
+  myservo.write(centro);
+
+  stop();
+}
+//192.168.217.95
+int getDistance(NewPing &sonar) {
+  int distance = sonar.ping_cm();
+  if (distance == 0) {
+    distance = MaxDistance;
+  }
+  return distance;
+}
+
+void giroIzquierda(){
+  myservo.write(160);
+  delay(500);
+  forward(250);
+  delay(miligiro);
+  stop();
+}
+
+void giroDerecha(){
+  myservo.write(80);
+  delay(500);
+  forward(250);
+  delay(miligiro);
+  stop();
+}
+
+int getAverageFrontDistance(int numReadings) {
+  int sum = 0;
+  
+  // Tomar múltiples lecturas y acumularlas
+  for (int i = 0; i < numReadings; i++) {
+    int distance = getDistance(sonarFront);
+    sum += distance;
+    delay(10); // Pequeño retraso entre lecturas para evitar interferencias
+  }
+
+  // Calcular el promedio de las lecturas
+  int averageDistance = sum / numReadings;
+  
+  return averageDistance;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int leftDistance = getDistance(sonarLeft);
+  int frontDistance = getDistance(sonarFront);
+  int rightDistance = getDistance(sonarRight);
 
+  Serial.print(leftDistance);
+  Serial.print("  ");
+  Serial.print(frontDistance);
+  Serial.print("  ");
+  Serial.println(rightDistance);
+
+
+
+  if (frontDistance > distanciafreno) {
+    if (getAverageFrontDistance(5) > distanciafreno){
+    myservo.write(centro);
+    delay(10);
+    forward(150);
+    }
+  }
+  else if (frontDistance < 100) {
+    forceStop();
+    stop();
+    delay(1000);
+    if (leftDistance > rightDistance){
+      giroIzquierda();
+    }
+    else if (leftDistance < rightDistance){
+      giroDerecha();
+    }
+    else{
+      giroIzquierda();
+    }
+  }
+
+
+  // Other logic for controlling the robot based on sensor data
 }
