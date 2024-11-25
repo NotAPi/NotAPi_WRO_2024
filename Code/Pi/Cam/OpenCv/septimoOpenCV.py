434ca04016e8c4b882fc71006d2a43f75eb49842
JaraGuillen <jaraguillen111@gmail.com>
2024-11-25 17:15:01 +0100

    commitmomento

--- Code/Pi/Cam/OpenCv/septimoOpenCV.py
+++ Code/Pi/Cam/OpenCv/septimoOpenCV.py
@@ -26,8 +26,8 @@ while True:
     p2_derecha = (anchura, altura) 
 
     hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    lower_red1 = np.array([120, 100, 100])
-    upper_red1 = np.array([140, 255, 255])
+    lower_red1 = np.array([125, 100, 100])
+    upper_red1 = np.array([130, 255, 255])
     #lower_red2 = np.array([160, 100, 100])
     #upper_red2 = np.array([179, 255, 255])
     lower_green = np.array([40, 100, 50])
